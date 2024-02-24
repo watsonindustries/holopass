b367ca33010322792d995c740bc54a790751ab3b
Daniils Petrovs <daniils@platogo.com>
2024-02-24 23:45:36 +0100

    Add privacy policy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,10 +23,11 @@
 	transition:fade={{ delay: 150, duration: 200 }}
 >
 	<div class="card space-y-4 bg-slate-50 p-6 shadow-lg" id="home-card">
-		<h1 class="mb-4 text-4xl font-bold tracking-tight">Welcome to holopass</h1>
+		<h1 class="mb-4 text-5xl font-bold tracking-tight text-center text-accent">holopass</h1>
 
 		<a href="/mypass/edit" class="btn btn-secondary rounded-full">Edit My Pass</a>
 		<a href="/login" class="btn btn-primary rounded-full">Login</a>
 		<button class="btn btn-primary rounded-full" on:click={handleLogout}>Logout</button>
+		<a href="/privacy" class="link link-primary text-center">Privacy</a>
 	</div>
 </section>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1 class="text-xl">Privacy Policy</h1>
+
+<p class="text-base">Effective Date: 24/02/2024</p>
+<p>
+	At holopass, accessible from https://holopass.pages.dev, one of our main priorities is the privacy
+	of our visitors. This Privacy Policy document contains types of information that are collected and
+	recorded by holopass and how we use it. General Data Protection Regulation (GDPR) We are a Data
+	Controller of your information. The legal basis for collecting and using the personal information
+	described in this Privacy Policy depends on the Personal Information we collect and the specific
+	context in which we collect the information:
+</p>
+<br />
+You have given holopass permission to do so 
+Processing your personal information is in holopass legitimate
+interests 
+holopass needs to comply with the law holopass will retain your personal information only for
+as long as is necessary for the purposes set out in this Privacy Policy. 
+We will retain and use your
+information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce
+our policies. Data Protection Rights under the General Data Protection Regulation (GDPR) If you are a
+resident of the European Economic Area (EEA), you have certain data protection rights. holopass aims
+to take reasonable steps to allow you to correct, amend, delete, or limit the use of your Personal Information.
+<p>
+    If you wish to be informed about what Personal Information we hold about you and if you want it to be
+    removed from our systems, please email us at dan@danirukun.com. In certain circumstances, you have the
+    following data protection rights: The right to access, update, or delete the information we have on you.
+    The right of rectification. The right to object. The right of restriction. The right to data portability
+    The right to withdraw consent    
+</p>
