fcf7aa1c5a52c5d5f436b4f05174d218fc7242b0
Daniils Petrovs <daniils@platogo.com>
2024-02-25 20:02:42 +0100

    Better discord login button styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,10 @@
 
 	export let data: PageData;
 
-	let { supabase } = data;
+	let { supabase, session } = data;
 	$: ({ supabase } = data);
+
+	$: isLoggedIn = session !== null;
 
 	async function handleLogout() {
 		const { error } = await supabase.auth.signOut();
@@ -23,11 +25,16 @@
 	transition:fade={{ delay: 150, duration: 200 }}
 >
 	<div class="card space-y-4 bg-slate-50 p-6 shadow-lg" id="home-card">
-		<h1 class="mb-4 text-5xl font-bold tracking-tight text-center text-accent">holopass</h1>
+		<h1 class="mb-4 text-center text-5xl font-bold tracking-tight text-accent">holopass</h1>
 
 		<a href="/mypass/edit" class="btn btn-primary rounded-full">Edit My Pass</a>
-		<a href="/login" class="btn btn-secondary rounded-full">Login</a>
-		<button class="btn btn-secondary rounded-full" on:click={handleLogout}>Logout</button>
+
+		{#if isLoggedIn}
+			<button class="btn btn-secondary rounded-full" on:click={handleLogout}>Logout</button>
+		{:else}
+			<a href="/login" class="btn btn-secondary rounded-full">Login</a>
+		{/if}
+
 		<a href="/privacy" class="link link-secondary text-center">Privacy</a>
 	</div>
 </section>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { fade } from 'svelte/transition';
+	import Icon from 'svelte-awesome';
+
+	import { faDiscord } from '@fortawesome/free-brands-svg-icons/faDiscord';
 
 	export let data: PageData;
 </script>
 
-<div class="card mx-auto mt-[40vh] flex w-96 items-center bg-slate-50 shadow-xl">
-	<div class="card-body items-center space-y-4">
-		<h2 class="card-title text-4xl text-accent font-bold tracking-tight">Sign In</h2>
-		<div class="card-actions justify-end">
-			<form class="" id="social-login" method="post">
-				<button formaction="?/login&provider=discord" class="btn btn-primary rounded-full"
-					>Login with Discord</button
-				>
-			</form>
+<section
+	id="home-container"
+	class="mx-4 mt-4 space-y-6 pb-36"
+	transition:fade={{ delay: 150, duration: 200 }}
+>
+	<div
+		class="card mx-auto mt-[40vh] flex items-center space-y-4 bg-slate-50 p-6 shadow-lg"
+		id="signin-card"
+	>
+		<div class="card-body items-center space-y-4">
+			<h2 class="card-title text-4xl font-bold tracking-tight text-secondary">Sign In</h2>
+			<div class="card-actions justify-end">
+				<form class="" id="social-login" method="post">
+					<button formaction="?/login&provider=discord" class="btn rounded-full bg-[#5865F2] text-white">
+						<Icon data={faDiscord} scale={1.2} />
+						Login with Discord
+					</button>
+				</form>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
