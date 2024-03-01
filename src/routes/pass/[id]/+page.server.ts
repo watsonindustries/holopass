8b8d6029d902a5d344820b94d8debac128087404
Daniils Petrovs <daniils@platogo.com>
2024-03-01 19:37:02 +0100

    Fix other pass views to use data streaming

--- src/routes/pass/[id]/+page.server.ts
+++ src/routes/pass/[id]/+page.server.ts
@@ -1,42 +1,20 @@
+import { fail } from '@sveltejs/kit';
+import { loadBadges, loadOshi, loadPass } from '../../../supabase';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params, locals }) => {
 	const { id } = params;
 	const { supabase } = locals;
 
-	const { data: pass, error } = await supabase
-		.from('profiles')
-		.select('avatar_url, bio, nickname, location, badge_ids, talent_ids')
-		.eq('id', id)
-		.single();
+	const pass = await loadPass(supabase)(id);
 
-	if (error) {
-		return error(500, error.message);
+	if (!pass) {
+		return fail(404, { message: 'Pass not found' });
 	}
 
-	const { data: badges, error2 } = await supabase
-		.from('badges')
-		.select('id, name, image, type')
-		.in('id', pass.badge_ids);
-
-	if (error2) {
-		return {
-			status: 500,
-			body: error
-		};
-	}
-
-	const { data: oshi, error: error3 } = await supabase
-		.from('talents')
-		.select('id, name_en, fanmark')
-		.in('id', pass.talent_ids);
-
-	if (error3) {
-		return {
-			status: 500,
-			body: error2
-		};
-	}
-
-	return { pass, badges, oshi };
+	return {
+		badges: loadBadges(pass?.badge_ids, supabase),
+		oshi: loadOshi(pass?.talent_ids, supabase),
+		pass
+	};
 }) satisfies PageServerLoad;
--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,10 @@
 import type { SupabaseClient, User } from '@supabase/supabase-js';
 
+/**
+ * Loads the profile of a user from the Supabase database.
+ * @param supabase - The Supabase client instance.
+ * @returns A function that accepts a user object and returns the user's profile.
+ */
 export function loadProfile(supabase: SupabaseClient) {
 	return async (user: User) => {
 		const { data: profile } = await supabase
@@ -11,12 +16,41 @@ export function loadProfile(supabase: SupabaseClient) {
 	};
 }
 
+/**
+ * Loads a pass from the Supabase database based on its ID.
+ * @param supabase - The Supabase client instance.
+ * @returns A function that accepts a pass ID and returns the pass data.
+ */
+export function loadPass(supabase: SupabaseClient) {
+	return async (id: string) => {
+		const { data: pass } = await supabase
+			.from('profiles')
+			.select('id, avatar_url, nickname, location, bio, badge_ids, talent_ids')
+			.eq('id', id)
+			.single();
+		return pass;
+	};
+}
+
+/**
+ * Loads badges from the Supabase database based on their IDs.
+ * @param badgeIds - An array of badge IDs.
+ * @param supabase - The Supabase client instance.
+ * @returns The badges data.
+ */
 export function loadBadges(badgeIds: number[], supabase: SupabaseClient) {
 	const badges = supabase.from('badges').select('id, name, image, type').in('id', badgeIds);
 	return badges;
 }
 
+/**
+ * Loads oshi (talents) from the Supabase database based on their IDs.
+ * @param talentIds - An array of talent IDs.
+ * @param supabase - The Supabase client instance.
+ * @returns The oshi data.
+ */
 export function loadOshi(talentIds: number[], supabase: SupabaseClient) {
 	const oshi = supabase.from('talents').select('id, name_en, fanmark').in('id', talentIds);
 	return oshi;
 }
+
