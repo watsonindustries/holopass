f3b126295a73765c37724a27167b36ca0ca1eed2
Daniils Petrovs <daniils@platogo.com>
2024-03-05 23:34:54 +0100

    Add Geologica font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Geologica"', ...defaultTheme.fontFamily.sans]
+			}
+		}
+	},
 	daisyui: {
 		themes: [
 			{
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import ServerUrlCopy from 'vite-plugin-url-copy'
+import ServerUrlCopy from 'vite-plugin-url-copy';
 
 export default defineConfig({
 	plugins: [sveltekit(), ServerUrlCopy({ qrcode: { disabled: false } })]
