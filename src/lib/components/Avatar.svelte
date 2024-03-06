07ac1bb387686894a0f7b1ada7c49baaa70f1857
Daniils Petrovs <daniils@platogo.com>
2024-03-07 00:07:41 +0100

    Set specific size on avatar images Fixes Avatar image does not load on first My Pass open #4

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -4,6 +4,6 @@
 
 <div class="avatar m-4">
 	<div class="w-full rounded-full ring ring-accent ring-offset-2 ring-offset-base-100">
-		<img src={avatarURL} alt="My avatar" />
+		<img src={avatarURL} alt="My avatar" width="128" height="128" />
 	</div>
 </div>
