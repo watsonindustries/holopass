20a437b4203af258b9f36c2b10af319ff2eb2264
Daniils Petrovs <daniils@platogo.com>
2024-03-10 00:18:00 +0100

    Use fallback img if avatar fails to load

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	const fallbackImg = 'https://cdn.discordapp.com/embed/avatars/1.png';
+
 	export let avatarURL = '';
 </script>
 
 <div class="avatar m-4">
 	<div class="w-full rounded-full ring ring-accent ring-offset-2 ring-offset-base-100">
-		<img src={avatarURL} alt="My avatar" width="128" height="128" loading="eager" />
+		<img src={avatarURL} alt="My avatar" width="128" height="128" loading="eager" on:error="{() => avatarURL = fallbackImg}" />
 	</div>
 </div>
