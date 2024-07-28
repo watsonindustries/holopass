61f3c0bb107f9e2d4978878030ad0ac70a664991
Daniils Petrovs <daniils@platogo.com>
2024-07-28 18:04:28 +0200

    Update QR code icon

--- src/lib/components/BottomNav.svelte
+++ src/lib/components/BottomNav.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import Icon from 'svelte-awesome';
-	import qrcode from 'svelte-awesome/icons/qrcode';
-
 	export let pathname = '/';
 </script>
 
@@ -20,7 +17,10 @@
 		<span class="btm-nav-label">My Pass</span>
 	</a>
 	<a class:active={pathname === '/qr'} href="/qr">
-		<Icon data={qrcode} scale={1.5} />
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
+			<path stroke-linecap="round" stroke-linejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z" />
+		</svg>
 		<span class="btm-nav-label">Connect</span>
 	</a>
 	<a class:active={pathname === '/about'} href="/about">
