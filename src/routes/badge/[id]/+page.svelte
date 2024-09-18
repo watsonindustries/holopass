dea28900b4ba632a4cf5effebe9c633bc9ef7e35
Kane Bruce <54819806+csm-kb@users.noreply.github.com>
2024-09-18 14:40:31 -0600

    page: badge/[id]: fix type errors + resolve sizing issue with people list element that occurred with few qty of list items

--- src/routes/badge/[id]/+page.svelte
+++ src/routes/badge/[id]/+page.svelte
@@ -8,55 +8,60 @@
 	export let data: PageData;
 
 	$: ({ badge, profilesForBadge } = data);
+
+	let image: string;
+
+	$: {
+		badge?.then((b) => {
+			image = b.image ?? BADGE_PLACEHOLDER_URL;
+		});
+	}
 </script>
 
 <section class="bg-neutral-50 p-4">
 	<BackButton url="/mypass" />
 </section>
 
-<div class="flex h-screen flex-col">
-	{#await badge then badge}
-		<div id="badge-container" class="flex flex-col items-center justify-center space-y-4 py-6">
-			<div class="avatar w-48" id="badge">
-				<div class="w-full rounded-full shadow-lg ring ring-neutral ring-offset-base-100">
-					<img
-						src={badge.image !== '' ? badge.image : BADGE_PLACEHOLDER_URL}
-						width="1000"
-						height="1000"
-						loading="lazy"
-						class:p-8={!badge.image}
-						class="mx-auto bg-base-100"
-						alt="Badge image"
-					/>
+<div class="flex min-h-[calc(100vh-128px)] flex-col">
+	{#await badge}
+		<p>Loading...</p>
+	{:then badge}
+		{#if badge}
+			<div id="badge-container" class="flex flex-col items-center justify-center space-y-4 py-6">
+				<div class="avatar w-48" id="badge">
+					<div class="w-full rounded-full shadow-lg ring ring-neutral ring-offset-base-100">
+						<img
+							src={image}
+							width="1000"
+							height="1000"
+							loading="lazy"
+							class:p-8={image === BADGE_PLACEHOLDER_URL}
+							class="mx-auto bg-base-100"
+							alt="Badge icon"
+						/>
+					</div>
 				</div>
-			</div>
-			<p class="text-balance text-center text-2xl font-semibold" id="badge-caption">{badge.name}</p>
-			{#if badge.external_url}
+				<p class="text-balance text-center text-2xl font-semibold" id="badge-caption">
+					{badge.name}
+				</p>
 				<a
 					href={badge.external_url}
 					class="btn btn-primary btn-lg min-w-52 rounded-full px-4 text-lg font-bold text-white shadow-lg"
 					>Website</a
 				>
-			{/if}
-		</div>
+			</div>
+			<div class="flex flex-col grow h-full w-full rounded-t-3xl bg-base-100 py-4 pb-10 shadow-lg">
+				<p class="p-8 text-center text-4xl font-bold tracking-tight text-secondary">
+					People with this badge
+				</p>
+				{#await profilesForBadge}
+					<ProfileItemListSkeleton />
+				{:then profiles}
+					<ProfileItemList {profiles}></ProfileItemList>
+				{/await}
+			</div>
+		{:else}
+			<p>Badge not found</p>
+		{/if}
 	{/await}
-
-	<div
-		class="flex w-full flex-grow flex-col rounded-3xl bg-base-100 py-4 pb-24 shadow-lg"
-		id="profiles-container"
-	>
-		<p class="p-8 text-center text-4xl font-bold tracking-tight text-secondary">
-			People with this badge
-		</p>
-		<div class="flex-grow space-y-4">
-			{#await profilesForBadge}
-				<ProfileItemListSkeleton />
-			{:then profiles}
-				<ProfileItemList {profiles}></ProfileItemList>
-			{/await}
-			<p class="text-center text-lg">
-				That's everyone! Ask more fans to add their badge to their holopass.
-			</p>
-		</div>
-	</div>
 </div>
