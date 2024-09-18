dd4914c60bb907656402b768e9e64c3c9551c92f
Kane Bruce <54819806+csm-kb@users.noreply.github.com>
2024-09-18 16:22:46 -0600

    supabase: add functions to migration script

--- supabase/migrations/20240916214339_add_geoloc_timing_to_badges.sql
+++ supabase/migrations/20240916214339_add_geoloc_timing_to_badges.sql
@@ -11,3 +11,79 @@ add column
 add column
     event_end timestamp with time zone
 ;
+
+-- add helper functions for Supabase querying (can't do proper queries directly)
+create or replace function get_nearest_badges(lat float, long float, max_dist float)
+returns table (
+  id public.badges.id%TYPE,
+  lat float,
+  long float,
+  dist_m float
+)
+language sql
+as
+$$
+select
+  id,
+  st_y(location::geometry) as lat,
+  st_x(location::geometry) as long,
+  st_distance(location, st_point(long, lat)::geography) as dist_m
+from public.badges
+where st_distance(location, st_point(long, lat)::geography) <= max_dist
+order by location <-> st_point(long, lat)::geography;
+$$;
+
+create or replace function get_nearest_badges_temporal(lat float, long float, max_dist float)
+returns table (
+  id public.badges.id%TYPE,
+  lat float,
+  long float,
+  dist_m float
+)
+language sql
+as
+$$
+select
+  id,
+  st_y(location::geometry) as lat,
+  st_x(location::geometry) as long,
+  st_distance(location, st_point(long, lat)::geography) as dist_m
+from public.badges
+where
+    st_distance(location, st_point(long, lat)::geography) <= max_dist
+    and now() between event_start and event_end
+order by location <-> st_point(long, lat)::geography;
+$$;
+
+create or replace function get_badge_location(id public.badges.id%TYPE)
+returns table (
+    id public.badges.id%TYPE,
+    lat float,
+    long float
+)
+language sql
+as
+$$
+select
+    id,
+    st_y(location::geometry) as lat,
+    st_x(location::geometry) as long
+from public.badges
+where id = $1;
+$$;
+
+create or replace function get_badge_locations()
+returns table (
+    id public.badges.id%TYPE,
+    lat float,
+    long float
+)
+language sql
+as
+$$
+select
+    id,
+    st_y(location::geometry) as lat,
+    st_x(location::geometry) as long
+from public.badges;
+$$;
