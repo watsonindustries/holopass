6f760ee8dce36b396a322b61b81bbe2f99708696
Kane Bruce <54819806+csm-kb@users.noreply.github.com>
2024-09-18 14:40:31 -0600

    page: badge/[id]: fix type errors + resolve sizing issue with people list element that occurred with few qty of list items

--- src/routes/badge/[id]/+page.server.ts
+++ src/routes/badge/[id]/+page.server.ts
@@ -1,13 +1,19 @@
-import type { PageServerLoad } from '../$types';
 import { loadBadge, loadProfilesWithBadge } from '../../../supabase';
+import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params, locals }) => {
 	const { id } = params;
 	const { supabase } = locals;
 
-	const badge = loadBadge(supabase, id);
+	// try convert id to number
+	const idAsNumber = Number(id);
+	if (isNaN(idAsNumber)) {
+		return { status: 404, error: new Error('Invalid badge ID') };
+	}
 
-	const profilesForBadge = loadProfilesWithBadge(supabase, id);
+	const badge = loadBadge(supabase, idAsNumber);
+
+	const profilesForBadge = loadProfilesWithBadge(supabase, idAsNumber);
 
 	return { badge, profilesForBadge };
 }) satisfies PageServerLoad;
--- src/routes/badge/[id]/+page.svelte
+++ src/routes/badge/[id]/+page.svelte
@@ -2,53 +2,66 @@
 	import BackButton from '$lib/components/BackButton.svelte';
 	import ProfileItemList from '$lib/components/ProfileItemList.svelte';
 	import ProfileItemListSkeleton from '$lib/components/ProfileItemListSkeleton.svelte';
-	import type { PageData } from '../$types';
+	import type { PageData } from './$types';
 	import { BADGE_PLACEHOLDER_URL } from '../../../const';
 
 	export let data: PageData;
 
 	$: ({ badge, profilesForBadge } = data);
 
-	$: image = badge?.image ?? BADGE_PLACEHOLDER_URL;
+	let image: string;
+
+	$: {
+		badge?.then((b) => {
+			image = b.image ?? BADGE_PLACEHOLDER_URL;
+		});
+	}
 </script>
 
 <section class="bg-neutral-50 p-4">
 	<BackButton url="/mypass" />
 </section>
 
-<div class="flex min-h-screen flex-col justify-between">
-	{#await badge then badge}
-		<div id="badge-container" class="flex flex-col items-center justify-center space-y-4 py-6">
-			<div class="avatar w-48" id="badge">
-				<div class="w-full rounded-full shadow-lg ring ring-neutral ring-offset-base-100">
-					<img
-						src={image}
-						width="1000"
-						height="1000"
-						loading="lazy"
-						class:p-8={image === BADGE_PLACEHOLDER_URL}
-						class="mx-auto bg-base-100"
-						alt="Badge image"
-					/>
+<div class="flex min-h-[calc(100vh-128px)] flex-col">
+	{#await badge}
+		<p>Loading...</p>
+	{:then badge}
+		{#if badge}
+			<div id="badge-container" class="flex flex-col items-center justify-center space-y-4 py-6">
+				<div class="avatar w-48" id="badge">
+					<div class="w-full rounded-full shadow-lg ring ring-neutral ring-offset-base-100">
+						<img
+							src={image}
+							width="1000"
+							height="1000"
+							loading="lazy"
+							class:p-8={image === BADGE_PLACEHOLDER_URL}
+							class="mx-auto bg-base-100"
+							alt="Badge icon"
+						/>
+					</div>
 				</div>
+				<p class="text-balance text-center text-2xl font-semibold" id="badge-caption">
+					{badge.name}
+				</p>
+				<a
+					href={badge.external_url}
+					class="btn btn-primary btn-lg min-w-52 rounded-full px-4 text-lg font-bold text-white shadow-lg"
+					>Website</a
+				>
 			</div>
-			<p class="text-balance text-center text-2xl font-semibold" id="badge-caption">{badge.name}</p>
-			<a
-				href={badge.external_url}
-				class="btn btn-primary btn-lg min-w-52 rounded-full px-4 text-lg font-bold text-white shadow-lg"
-				>Website</a
-			>
-		</div>
+			<div class="flex flex-col grow h-full w-full rounded-t-3xl bg-base-100 py-4 pb-10 shadow-lg">
+				<p class="p-8 text-center text-4xl font-bold tracking-tight text-secondary">
+					People with this badge
+				</p>
+				{#await profilesForBadge}
+					<ProfileItemListSkeleton />
+				{:then profiles}
+					<ProfileItemList {profiles}></ProfileItemList>
+				{/await}
+			</div>
+		{:else}
+			<p>Badge not found</p>
+		{/if}
 	{/await}
-
-	<div class="h-full w-full rounded-t-3xl bg-base-100 py-4 pb-10 shadow-lg">
-		<p class="p-8 text-center text-4xl font-bold tracking-tight text-secondary">
-			People with this badge
-		</p>
-		{#await profilesForBadge}
-			<ProfileItemListSkeleton />
-		{:then profiles}
-			<ProfileItemList {profiles}></ProfileItemList>
-		{/await}
-	</div>
 </div>
