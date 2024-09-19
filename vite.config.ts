5aaa72e2b8169fac3a066d96bc679997c0a3623a
Kane Bruce <54819806+csm-kb@users.noreply.github.com>
2024-09-18 18:04:21 -0600

    package: add sveaflet + TS typing + isomorphic import for map lib SSR client-side compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import ServerUrlCopy from 'vite-plugin-url-copy';
+import { isoImport } from 'vite-plugin-iso-import';
 
 export default defineConfig({
-	plugins: [sveltekit(), ServerUrlCopy({ qrcode: { disabled: false } })]
+	plugins: [
+		sveltekit(),
+		ServerUrlCopy({ qrcode: { disabled: false } }),
+		isoImport()
+	]
 });
