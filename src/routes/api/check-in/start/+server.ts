6f2a2463d8c1583d6a34ea36b91f67210c753a2b
Kane Bruce <54819806+csm-kb@users.noreply.github.com>
2024-09-29 04:15:03 -0600

    proto: add POST /api/check-in/start

---
+++ src/routes/api/check-in/start/+server.ts
@@ -0,0 +1,60 @@
+import { json } from '@sveltejs/kit';
+
+import { loadNearestBadgesTemporal } from '../../../../supabase';
+import type { Database } from '$lib/database.types.js';
+
+export type CheckInStartPostRequestBody = {
+	/** GPS coordinates, in `[latitude, longitude]`. */
+	loc: [number, number];
+};
+export type CheckInStartPostResponseBody = {
+	badges?: Database['public']['Functions']['get_nearest_badges_temporal']['Returns'];
+	error?: string;
+};
+export async function POST({ request, locals: { supabase, getSession } }) {
+	const session = await getSession();
+	if (!session) {
+		return json({
+			error: 'Unauthorized',
+		} as CheckInStartPostResponseBody, {
+			status: 401,
+		});
+	}
+
+	let body: CheckInStartPostRequestBody;
+	try {
+		body = await request.json();
+	} catch (e) {
+		const errMsg = e instanceof Error ? e.message : 'Unknown error';
+		return json({
+			error: `Invalid request: ${errMsg}`,
+		} as CheckInStartPostResponseBody, {
+			status: 400,
+		});
+	}
+
+	const { loc } = body;
+	const res = await loadNearestBadgesTemporal(supabase, loc[0], loc[1], 1600 /* 1-mile radius */);
+
+	if (!res) {
+		return json({
+			error: 'Internal server error',
+		} as CheckInStartPostResponseBody, {
+			status: 500,
+		});
+	}
+
+	if (res.length === 0) {
+		return json({
+			error: 'No badges found for current time and location',
+		} as CheckInStartPostResponseBody, {
+			status: 404,
+		});
+	}
+
+	return json({
+		badges: res,
+	} as CheckInStartPostResponseBody, {
+		status: 200,
+	});
+}
