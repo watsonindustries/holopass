3f688a12a23fc681263222ad60187d2213b9afa6
Kane Bruce <54819806+csm-kb@users.noreply.github.com>
2024-10-06 23:55:18 -0600

    check-in: implement light PopupStatusCard component for user-friendly check-in flow status messages

---
+++ src/lib/components/PopupStatusCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Icon from 'svelte-awesome';
+	import type { IconType } from 'svelte-awesome/components/Icon.svelte';
+
+	export let icon: IconType;
+	export let color: string;
+	export let text_color: string;
+</script>
+
+<div class={`flex-row card bg-${color}-600 p-2 text-${text_color} place-content-center text-center`}>
+	<div class="align-center">
+		<Icon data={icon} scale={1.2}></Icon>
+	</div>
+	<div class="px-2" />
+	<slot />
+</div>
