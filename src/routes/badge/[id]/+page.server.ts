0fd78b6e809d6c273f1a9cc00cf266873d3c802e
Daniils Petrovs <daniils@streamelements.com>
2024-10-15 19:35:42 +0200

    Closes Badge image might not appear in badge route #43

--- src/routes/badge/[id]/+page.server.ts
+++ src/routes/badge/[id]/+page.server.ts
@@ -1,13 +1,13 @@
-import type { PageServerLoad } from '../$types';
+import type { PageServerLoad } from './$types';
 import { loadBadge, loadProfilesWithBadge } from '../../../supabase';
 
 export const load = (async ({ params, locals }) => {
 	const { id } = params;
 	const { supabase } = locals;
 
-	const badge = loadBadge(supabase, id);
+	const badge = loadBadge(supabase, Number(id));
 
-	const profilesForBadge = loadProfilesWithBadge(supabase, id);
+	const profilesForBadge = loadProfilesWithBadge(supabase, Number(id));
 
 	return { badge, profilesForBadge };
 }) satisfies PageServerLoad;
--- src/routes/badge/[id]/+page.svelte
+++ src/routes/badge/[id]/+page.svelte
@@ -2,53 +2,61 @@
 	import BackButton from '$lib/components/BackButton.svelte';
 	import ProfileItemList from '$lib/components/ProfileItemList.svelte';
 	import ProfileItemListSkeleton from '$lib/components/ProfileItemListSkeleton.svelte';
-	import type { PageData } from '../$types';
+	import type { PageData } from './$types';
 	import { BADGE_PLACEHOLDER_URL } from '../../../const';
 
 	export let data: PageData;
 
 	$: ({ badge, profilesForBadge } = data);
-
-	$: image = badge?.image ?? BADGE_PLACEHOLDER_URL;
 </script>
 
 <section class="bg-neutral-50 p-4">
 	<BackButton url="/mypass" />
 </section>
 
-<div class="flex min-h-screen flex-col justify-between">
+<div class="flex h-screen flex-col">
 	{#await badge then badge}
 		<div id="badge-container" class="flex flex-col items-center justify-center space-y-4 py-6">
 			<div class="avatar w-48" id="badge">
 				<div class="w-full rounded-full shadow-lg ring ring-neutral ring-offset-base-100">
 					<img
-						src={image}
+						src={badge.image !== '' ? badge.image : BADGE_PLACEHOLDER_URL}
 						width="1000"
 						height="1000"
 						loading="lazy"
-						class:p-8={image === BADGE_PLACEHOLDER_URL}
+						class:p-8={!badge.image}
 						class="mx-auto bg-base-100"
 						alt="Badge image"
 					/>
 				</div>
 			</div>
 			<p class="text-balance text-center text-2xl font-semibold" id="badge-caption">{badge.name}</p>
-			<a
-				href={badge.external_url}
-				class="btn btn-primary btn-lg min-w-52 rounded-full px-4 text-lg font-bold text-white shadow-lg"
-				>Website</a
-			>
+			{#if badge.external_url}
+				<a
+					href={badge.external_url}
+					class="btn btn-primary btn-lg min-w-52 rounded-full px-4 text-lg font-bold text-white shadow-lg"
+					>Website</a
+				>
+			{/if}
 		</div>
 	{/await}
 
-	<div class="h-full w-full rounded-t-3xl bg-base-100 py-4 pb-10 shadow-lg">
+	<div
+		class="flex w-full flex-grow flex-col rounded-3xl bg-base-100 py-4 pb-24 shadow-lg"
+		id="profiles-container"
+	>
 		<p class="p-8 text-center text-4xl font-bold tracking-tight text-secondary">
 			People with this badge
 		</p>
-		{#await profilesForBadge}
-			<ProfileItemListSkeleton />
-		{:then profiles}
-			<ProfileItemList {profiles}></ProfileItemList>
-		{/await}
+		<div class="flex-grow space-y-4">
+			{#await profilesForBadge}
+				<ProfileItemListSkeleton />
+			{:then profiles}
+				<ProfileItemList {profiles}></ProfileItemList>
+			{/await}
+			<p class="text-center text-lg">
+				That's everyone! Ask more fans to add their badge to their holopass.
+			</p>
+		</div>
 	</div>
 </div>
