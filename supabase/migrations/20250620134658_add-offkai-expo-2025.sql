4f9e612963591cc0a936c54c6f77ad69e477435b
Daniils Petrovs <daniils@streamelements.com>
2025-06-20 09:15:08 -0700

    Add SQL migration for OffKai Expo 2025 event badges

---
+++ supabase/migrations/20250620134658_add-offkai-expo-2025.sql
@@ -0,0 +1,10 @@
+insert into public.badges
+  (name, type, location, external_url, event_start, event_end)
+values
+  ('OffKai Expo 2025',
+    'convention',
+    st_point(-121.8863, 37.3297),
+    'https://www.offkaiexpo.com/',
+    '2025-06-20 17:00:00+00:00'::timestamp,
+    '2025-06-23 01:00:00+00:00'::timestamp)
+ON CONFLICT (id) DO NOTHING;
